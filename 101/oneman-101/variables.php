ec4984462c497e6502c5f6c4e9d4c846445cf7b5
one-man1 <mesheel57rashed@iFaisal.local>
2017-03-18 11:26:20 +0300

    variables rules in php

---
+++ 101/oneman-101/variables.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ *  ((variables in php))     المتغيرات
+
+* Rules for PHP variables:   شروط تسمية المتغيرات
+
+- A variable starts with the $ sign, followed by the name of the variable
+لابد ان يبدأ المتغير بعلامة $ ويتبعه الاسم مباشرة
+- A variable name must start with a letter or the underscore character
+اسم المتغير لابدا ان يبدآ بحرف او شرطة سفلية
+- A variable name cannot start with a number
+لا يمكن يبدأ اسم المتغير برقم
+- A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+الاسم يقتصر علر الحدوف والارقام والشرطة السفلية بمعنى لا يمكن استخدام اي علامات اخرى
+- Variable names are case-sensitive ($age and $AGE are two different variables)
+اسم المتغير حساس للاحرف الكبيرة والصغيرة اي لابد من تطابق تام في الاحرف الكبيرة والصغيرة
+
+ */
+
+    // Examlpe    مثال على تسمية متغير تنطبق عليه الشروط
+$welcome_1 = " welcome in hour withe programmer ";
+
+echo "$welcome_1";
