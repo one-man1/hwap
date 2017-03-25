3fa665ad3b1cc83b157db78703c3825df504df09
faisal o <faisalo@ifaisal.local>
2017-03-25 18:09:19 +0300

    task 5 Separate PHP logic Frorm Presntation

---
+++ 101/oneman-101/task5-1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Task</title>
+    <style>
+        header{
+            background: rgba(52,17,40,0.61);
+            text-align: center;
+            padding: 1em;
+           font-size:2em;
+            border: double;
+
+            text-decoration-color: cornflowerblue;
+        }
+    </style>
+</head>
+<body>
+<header>
+<h1 style="color: cornsilk ">
+   <?= $oneman;?>
+</h1></header>
+</body>
+</html
