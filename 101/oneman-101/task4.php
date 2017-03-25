189bb63439a0997ae45522d6a71dc00b84db562c
faisal o <faisalo@ifaisal.local>
2017-03-25 18:00:12 +0300

    task 4 PHP and HTML

---
+++ 101/oneman-101/task4.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <tatal>task 4</tatal>
+    <style>
+        header{padding =2em;
+            text-align = center;}
+
+
+    </style>
+</head>
+
+<body>
+<header>
+    <h1>
+        <?php
+        $welcome= "welcome to hour with a programmer ";
+echo "hello, $welcome";
+?>
+    </h1>
+</header>
+
+</body>
+
+</html>
